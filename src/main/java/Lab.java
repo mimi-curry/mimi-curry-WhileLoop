da2f8fdb4ddf39cda0a38b2fc6113d17c0a655f1
mimi-curry <Mimi8400114@gmail.com>
2023-02-25 02:27:31 +0000

    commit

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -37,7 +37,13 @@ public class Lab {
         int i = 0;
 
 //        code here
+        while (i < end){
+            sum = sum + i;
+            i = i + 1;
+        }
+            return sum;
+        
 
-        return sum;
+        
     }
 }
